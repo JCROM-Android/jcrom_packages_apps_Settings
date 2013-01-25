0078806b882f7932d98a0e069aa1a7d33a98a321
Akihiro Maeda <sola.1980.a@gmail.com>
2013-01-26 00:16:21 +0900

    Add JCROM theme setting.
    - ATOK

--- src/com/android/settings/ThemeManager.java
+++ src/com/android/settings/ThemeManager.java
@@ -45,6 +45,7 @@ public class ThemeManager {
             "statusbar",
             "navibar",
             "simeji",
+            "atok",
             "sounds/effect",
             "sounds/bootsound",
             "wallpaper",
